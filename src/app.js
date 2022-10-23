3db317188eaad7effacf05bb0e68535320694e90
mxleach <max.leach208@gmail.com>
2022-10-23 16:18:40 -0700

    update

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import './app.css';
 
 function App() {
   return (
-   <BrowserRouter>
+   <BrowserRouter basename="/bad-bank">
       <Navbar />
       <UserContext.Provider value={{users:[{name:'Max',email:'max@mit.edu',password:'secret',balance:1000}]}}>
 
