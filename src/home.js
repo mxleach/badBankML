8f340d4d2357ec71d929e565dcf94e5bcf1bfaa1
mxleach <max.leach208@gmail.com>
2022-10-24 13:42:37 -0700

    Update home.js

--- src/home.js
+++ src/home.js
@@ -8,7 +8,7 @@ function Home(){
         <h5 className="card-title"  style={{fontSize: 30 + "px", fontWeight: 'bold', color: "navy"}}>Welcome to Bad Bank</h5>
           <p className="card-text" style={{fontSize: 12 + "px", color: "navy" }}>Thank you for choosing us!</p>
          
-          <img src="/images/bank.png" className="card-img-top" alt="globe" id="homeImg" style={{width: 500 + 'px', position: 'relative',}}/>
+          <img src="/images/bank.png" className="card-img-top" alt="globe" id="homeImg" /*style={{width: 500 + 'px', position: 'relative',}}*//>
         <div className="card-body" id="homeBody">
     
           <Link to="/create-account"><button type="submit" className="btn btn-primary" id="createAcctBtn" style={{fontSize: 20 + "px", fontWeight: 'bold', backgroundColor: "navy", opacity: .85}}>Join Bad Bank</button></Link>
